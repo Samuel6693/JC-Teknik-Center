ca90b10f51c996e79d30e5ff197495e1528ea5b0
Samuel <samuel_somy93@hotmail.com>
2026-06-11 13:16:51 +0200

    feat: add shop product data

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,8 +1,13 @@
+import { productCategories, products } from "../data/products"
+
 function Shop() {
   return (
     <section className="page-section">
       <h1>Butik</h1>
-      <p>Välkommen till vår butik!</p>
+      <p>
+        Webbshoppen innehåller {products.length} demo-produkter i{" "}
+        {productCategories.length} kategorier.
+      </p>
     </section>
   )
 }
